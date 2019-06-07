a93f151e3232793566c576db22424eae1f93c0da
Mohammad Zaki <mohdzaki407@gmail.com>
2019-06-07 23:33:29 +0530

    Added Git Ignore and Minor Changes

--- static/js/scripty.js
+++ static/js/scripty.js
@@ -1,1 +1,10 @@
-  $.material.init();
+
+$(document).ready(function(){
+    console.log("Loaded");
+    $.material.init();
+
+    $(document).on("submit" , "#register-form",function(e){
+        e.preventDefault();
+        console.log("form submitted");
+        });
+});
